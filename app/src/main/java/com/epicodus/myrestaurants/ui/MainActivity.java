6cea94457db067fe1e9b811fbb5f4c4a2bac4c8b
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-19 11:41:12 +0300

    I have added the method that allows us to save locations to firebase

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -14,6 +14,11 @@ import android.widget.TextView;
 
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.apache.http.impl.conn.LoggingSessionOutputBuffer;
 
@@ -21,8 +26,10 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+    //private SharedPreferences mSharedPreferences;
+    //private SharedPreferences.Editor mEditor;
+
+    private DatabaseReference mSearchedLocationReference;
 
 
 
@@ -32,12 +39,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSearchedLocationReference=FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+        //mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //mEditor = mSharedPreferences.edit();
+
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
 
@@ -48,17 +62,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
-            if(!(location).equals("")) {
-                addToSharedPreferences(location);
-            }
+            saveLocationToFirebase(location);
+            //if(!(location).equals("")) {
+                //addToSharedPreferences(location);
+            //}
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
             startActivity(intent);
         }
     }
-              //takes user inputed location as an argument
-    public void addToSharedPreferences(String location){
-        //calls upon the editor to add this info to shared preferences
-        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-        //values passed are the contant stored in constants file and the argument
+
+    public void saveLocationToFirebase(String location){
+        mSearchedLocationReference.push().setValue(location);
     }
+              //takes user inputed location as an argument
+    //public void addToSharedPreferences(String location){
+        //calls upon the editor to add this info to shared preferences
+        //mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+        //values passed are the contant stored in constants file and the argument
+    //}
 }
