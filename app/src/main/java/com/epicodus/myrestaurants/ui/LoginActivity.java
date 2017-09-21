30bae4d7cddd7b64eb01f5afebdff9e7429f0ce8
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-21 09:44:00 +0300

    I have added the intent to launch the create account activity and passed the method to the textview in the log in activity

--- app/src/main/java/com/epicodus/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.epicodus.myrestaurants.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.myrestaurants.R;
 
+import butterknife.Bind;
+
 public class LoginActivity extends AppCompatActivity {
+@Bind(R.id.registerTextView)
+    TextView mRegisteraccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void registerAccount (View view){
+        Intent intent=new Intent(this,CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
